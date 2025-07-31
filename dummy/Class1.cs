4551d3c6f87ee0064c72e21d8de714314d70cc1c
Sanket Kokane <168641049+sako-milestone@users.noreply.github.com>
2025-07-31 10:56:37 +0200

    XPCO-added solution

---
+++ dummy/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dummy
+{
+    public class Class1
+    {
+        Console.WriteLine("Hello, World!"); 
+    }
+}
