6fcc391037b7d11c4a35cc0e5f286e7e0454fe45
sako-milestone <168641049+sako-milestone@users.noreply.github.com>
2025-08-25 16:43:16 +0200

    3 adding samples (#13)
    * removed console project
    
    * 3-added-audiodemo-diles
    
    * 3-added new package reference
    
    * updated reference
    
    * added new samples

---
+++ dummy/AudioDemo/README.md
@@ -0,0 +1,38 @@
+---
+description: The AudioDemo sample shows how a microphone can be selected
+  and the use of the AudioPlayerControl. The login credentials used in
+  this sample is a current Windows credentials.
+keywords: Component integration
+lang: en-US
+title: Audio Demo
+---
+
+# Audio Demo
+
+The AudioDemo sample shows how a microphone can be selected and the use
+of the AudioPlayerControl. The login credentials used in this sample is
+a current Windows credentials.
+
+When a microphone has been selected the incoming audio from that
+microphone is played on the PC\'s speakers.
+
+![Audio Player](AudioSample.png)
+
+## The sample demonstrates
+
+- Initialization of MIP .NET Library, when audio playback is required
+- How to use the ItemPickerForm to select a microphone
+
+## Using
+
+- VideoOS.Platform.ClientControl
+- VideoOS.Platform.UI.ItemPickerForm
+- VideoOS.Platform.UI.AudioPlayerControl
+
+## Environment
+
+- MIP .NET library
+
+## Visual Studio C\# project
+
+- [AudioDemo.csproj](javascript:clone('https://github.com/milestonesys/mipsdk-samples-component','src/ComponentSamples.sln');)
---
+++ dummy/LogOnServer/LogResourceHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using VideoOS.Platform;
+using VideoOS.Platform.Log;
+
+namespace LogOnServer
+{
+    internal class LogResourceHandler
+    {
+        private const string _myApplication = "LogMessageTester";
+        private const string _myComponent = "LogForm";
+
+        private const string _id1 = "RecStart";
+        private const string _id2 = "RecStop";
+        private const string _id3 = "ACSwipe";
+
+        internal static void RegisterMyMessages()
+        {
+            LogMessageDictionary lmd1 = new LogMessageDictionary("en-US", "3.9", _myApplication, _myComponent,
+                                                                BuildDictionary("en-US"), "AccessControl");
+            LogMessageDictionary lmd2 = new LogMessageDictionary("da-DK", "3.9", _myApplication, _myComponent,
+                                                                BuildDictionary("da-DK"), "AccessControl");
+            VideoOS.Platform.Log.LogClient.Instance.RegisterDictionary(lmd1);
+            VideoOS.Platform.Log.LogClient.Instance.RegisterDictionary(lmd2);
+        }
+
+        private static Dictionary<String, LogMessage> BuildDictionary(string culture)
+        {
+            Dictionary<string, LogMessage> messages = new Dictionary<string, LogMessage>();
+            Strings.Culture = new CultureInfo(culture);
+            messages.Add(_id1, new LogMessage() { Id = _id1, Category = "ExternalComponents", Message = Strings.RecStart, Group=Group.System, Severity = Severity.Info, Status = Status.StatusQuo, RelatedObjectKind = Kind.Camera });
+            messages.Add(_id2, new LogMessage() { Id = _id2, Category = "ExternalComponents", Message = Strings.RecStop, Group = Group.System, Severity = Severity.Info, Status = Status.StatusQuo, RelatedObjectKind = Kind.Camera });
+            messages.Add(_id3, new LogMessage() { Id = _id3, Category = "ExternalComponents", Message = Strings.ACSwipe, Group = Group.Audit, Severity = Severity.Info, Status = Status.StatusQuo, RelatedObjectKind = Kind.Server });
+            return messages;
+        }
+
+        internal static void LogStart(Item cameraItem)
+        {
+            Dictionary<string, string> parms = new Dictionary<string, string>();
+            parms["c1"] = cameraItem.Name;      // Filling for the {c1} part of the message
+
+            VideoOS.Platform.Log.LogClient.Instance.NewEntry(_myApplication, _myComponent, _id1, cameraItem, parms);
+        }
+
+        internal static void LogStop(Item cameraItem)
+        {
+            Dictionary<string, string> parms = new Dictionary<string, string>();
+            parms["c1"] = cameraItem.Name;      // Filling for the {c1} part of the message
+
+            VideoOS.Platform.Log.LogClient.Instance.NewEntry(_myApplication, _myComponent, _id2, cameraItem, parms);
+        }
+
+        internal static void CardSwiped(bool granted)
+        {
+            Dictionary<string, string> parms = new Dictionary<string, string>();
+            parms["d1"] = "Door 25 ";
+            parms["card"] = "ABCs card";
+
+            Item item = new Item(EnvironmentManager.Instance.MasterSite, "Door 25-1");
+
+            VideoOS.Platform.Log.LogClient.Instance.AuditEntry(_myApplication, _myComponent, _id3, item, parms, granted ? PermissionState.Granted : PermissionState.Denied);
+        }
+    }
+}
